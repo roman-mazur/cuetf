96320ee0852b41d5bb3b0d8f49e804655de87a62
Roman Mazur <roman@rmazur.io>
2026-03-22 14:45:19 +0100

    internal/ci: make it clear that yaml files are generated

--- internal/ci/generate.go
+++ internal/ci/generate.go
@@ -1,18 +1,18 @@
 package ci
 
 // Tests.
-//go:generate cue export -e workflows.test --out=yaml -f -o ../../.github/workflows/test.yml
+//go:generate cue export -e workflows.test --out=yaml -f -o ../../.github/workflows/test_gen.yml
 
 // Analysis of the cloud resources evolution.
-//go:generate cue export -e workflows.analysis --out=yaml -f -o ../../.github/workflows/analysis.yaml
+//go:generate cue export -e workflows.analysis --out=yaml -f -o ../../.github/workflows/analysis_gen.yaml
 
 // Terraform provider updates.
-//go:generate cue export -e workflows.regenerate --out=yaml -f -o ../../.github/workflows/regenerate.yml
-//go:generate cue export -e workflows.dependabot --out=yaml -f -o ../../.github/workflows/dependabot.yml
+//go:generate cue export -e workflows.regenerate --out=yaml -f -o ../../.github/workflows/regenerate_gen.yml
+//go:generate cue export -e workflows.dependabot --out=yaml -f -o ../../.github/workflows/dependabot_gen.yml
 //go:generate cue export -e dependabot --out=yaml -f -o ../../.github/dependabot.yaml
 
 // Metadata.
-//go:generate cue export -e workflows.metadata --out=yaml -f -o ../../.github/workflows/metadata.yml
+//go:generate cue export -e workflows.metadata --out=yaml -f -o ../../.github/workflows/metadata_gen.yml
 
 // Automatic release.
-//go:generate cue export -e workflows.autorelease --out=yaml -f -o ../../.github/workflows/autorelease.yml
+//go:generate cue export -e workflows.autorelease --out=yaml -f -o ../../.github/workflows/autorelease_gen.yml
